8a5f8cc2f2adf36b3328b642e3332f5cde9b8927
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2018-12-20 20:03:22 +0100

    Falta algo en Controller Activity

--- app/src/main/java/com/example/jcmilena/apptomodify1/ControllerActivity.java
+++ app/src/main/java/com/example/jcmilena/apptomodify1/ControllerActivity.java
@@ -5,7 +5,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ControllerActivity extends AppCompatActivity  {
+public class ControllerActivity extends AppCompatActivity  implements EasyFragment.FragmentListener {
 
     String fragment_number = "1";
     int fragment_color = R.color.colorFragment1;
@@ -16,11 +16,10 @@ public class ControllerActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
-
-
+        
         FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = EasyFragment.newInstance(fragment_number, fragment_color);
         fm.beginTransaction().replace(fragment_container, fragment,fragment_tag).commit();
-
     }
 
     @Override
@@ -29,11 +28,32 @@ public class ControllerActivity extends AppCompatActivity  {
             FragmentManager fm = getSupportFragmentManager();
             Fragment fragmentToRemove = fm.findFragmentByTag("Frag1");
             fm.beginTransaction().remove(fragmentToRemove).commit();
-        }
-        else{
+            fragment_number = "2";
+            fragment_color = R.color.colorFragment2;
+            fragment_container = R.id.fragment_container2;
+            fragment_tag = "Frag2";
+            Fragment fragment = EasyFragment.newInstance(fragment_number, fragment_color);
+            fm.beginTransaction().replace(fragment_container, fragment, fragment_tag).remove(fragmentToRemove).commit();
 
+        } else {
+            FragmentManager fm = getSupportFragmentManager();
+            Fragment fragmentToRemove = fm.findFragmentByTag("Frag2");
+            fm.beginTransaction().remove(fragmentToRemove).commit();
+            fragment_number = "1";
+            fragment_color = R.color.colorFragment1;
+            fragment_container = R.id.fragment_container1;
+            fragment_tag = "Frag1";
+            Fragment fragment = EasyFragment.newInstance(fragment_number, fragment_color);
+            fm.beginTransaction().replace(fragment_container, fragment, fragment_tag).remove(fragmentToRemove).commit();
         }
-
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("numero", fragment_number);
+        outState.putInt("color",fragment_color);
+        outState.putInt("container", fragment_container);
+        outState.putString("tag", fragment_tag);
+    }
 }
--- app/src/main/java/com/example/jcmilena/apptomodify1/EasyFragment.java
+++ app/src/main/java/com/example/jcmilena/apptomodify1/EasyFragment.java
@@ -1,40 +1,35 @@
 package com.example.jcmilena.apptomodify1;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
-
 
 public class EasyFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "numero";
-    private static final String ARG_PARAM2 = "color";
+    private static final String numberArgument = "numero";
+    private static final String colorArgument = "color";
 
     // TODO: Rename and change types of parameters
-    private String mParam1 ="1";
-    private int mParam2 = R.color.colorBlanco;
-
-
+    private String firstNumber ="1";
+    private int firstColor = R.color.colorBlanco;
+    FragmentListener myListener;
 
     public EasyFragment() {
         // Required empty public constructor
     }
 
-
     // TODO: Rename and change types and number of parameters
-    public static EasyFragment newInstance(String param1) {
+    public static EasyFragment newInstance(String number, int color) {
         EasyFragment fragment = new EasyFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        fragment.setArguments(args);
+        Bundle myArguments = new Bundle();
+        myArguments.putString(numberArgument, number);
+        myArguments.putInt(colorArgument, color);
+        fragment.setArguments(myArguments);
         return fragment;
     }
 
@@ -42,28 +37,26 @@ public class EasyFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
+            firstNumber = getArguments().getString(numberArgument);
+            firstColor = getArguments().getInt(colorArgument);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_easy, container, false);
-
         TextView textView = (TextView) view.findViewById(R.id.text_fragment);
 
-        textView.setText("Soy el fragment "+ mParam1);
-        textView.setBackgroundResource(mParam2);
+        textView.setText("Soy el fragment "+ firstNumber);
+        textView.setBackgroundResource(firstColor);
 
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Acció a fer
+                myListener.pulsado(firstNumber);
                 }
         });
-
 
         return view;
     }
@@ -71,9 +64,16 @@ public class EasyFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
+        myListener = (FragmentListener)getActivity();
     }
 
+    @Override public void onDetach() {
+        super.onDetach();
+    }
+
+    public interface FragmentListener {
+        public void pulsado (String numero);
+    }
 
 
 }
